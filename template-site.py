faf84d9bb49f3549b29b24c37c5ed147970b8d2e
Davi-Possa <daviguimaraespossa@gmail.com>
2022-11-24 09:56:03 -0300

    Ultima

--- template-site.py
+++ template-site.py
@@ -11,9 +11,32 @@ def main():
     def main_page():
         return flask.render_template(
             'template3.html',
-             meu_novo_paragrafo='<p>Toma deles</p>'
+             meu_novo_paragrafo='<p>Brasil espanca Sérvia</p>'
         )
 
+    @app.route('/copa', methods=['GET'])
+    def pagina_da_copa():
+        return flask.render_template(
+            'template4.html',
+            meu_novo_paragrafo='<p>Brasil espanca Sérvia</p>'
+        )
+
+    @app.errorhandler(404)
+    def page_not_found(page):
+        return flask.render_template('template404.html'), 404
+
+
+    @app.route('/jogadores', methods=['POST'])
+    def jogadores():
+        jogador = flask.request.form.get('selecionado')
+        print('o jogador selecionado foi', jogador)
+
+        response = flask.jsonify({'jogador':"Richarlison"})
+        response.headers.add('Acess-Control-Allow-Origin', '*')
+        return response
+
+    app.register_error_handler(404, page_not_found)      
+        
     app.run(debug = True)
 
 
