919cc626f70d41eab16f9644e5b17f16367b4c22
Davi-Possa <daviguimaraespossa@gmail.com>
2022-12-01 09:44:41 -0300

    Toma deles

--- FODA/JS/template2.js
+++ FODA/JS/template2.js
@@ -1,43 +1,141 @@
-function startTimer(duration, display) {
+const FULL_DASH_ARRAY = 283;
+const WARNING_THRESHOLD = 10;
+const ALERT_THRESHOLD = 5;
 
-  var timer = duration, horas = 0, minutos = 1, segundos;
-  const tempo = document.getElementById("#tempo");
+const COLOR_CODES = {
+  info: {
+    color: "green"
+  },
+  warning: {
+    color: "orange",
+    threshold: WARNING_THRESHOLD
+  },
+  alert: {
+    color: "red",
+    threshold: ALERT_THRESHOLD
+  }
+};
 
-  setInterval(function(){
-    horas = parseInt(horas);
-    minutos = parseInt(minutos);
-    segundos = parseInt(timer % 60, 10);
+const TIME_LIMIT = 15;
+let timePassed = 0;
+let timeLeft = TIME_LIMIT;
+let timerInterval = null;
+let remainingPathColor = COLOR_CODES.info.color;
 
-    if(segundos == 0 && minutos > 0){
-      minutos = minutos - 1;
-      segundos = 59
-    }
+document.getElementById("app").innerHTML = `
+<div class="base-timer">
+  <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+    <g class="base-timer__circle">
+      <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
+      <path
+        id="base-timer-path-remaining"
+        stroke-dasharray="283"
+        class="base-timer__path-remaining ${remainingPathColor}"
+        d="
+          M 50, 50
+          m -45, 0
+          a 45,45 0 1,0 90,0
+          a 45,45 0 1,0 -90,0
+        "
+      ></path>
+    </g>
+  </svg>
+  <span id="base-timer-label" class="base-timer__label">${formatTime(
+    timeLeft
+  )}</span>
+</div>
+`;
 
-    if(minutos == 0 && horas > 0){
-      horas = horas - 1;
-      minutos = 59
-    }
-
-    if(minutos == 0 && segundos == 0 ){
-      alert("Acabou o tempo");
-    }
-
-    horas = horas < 10 ? "0" + horas : horas;
-    minutos = minutos < 10 ? "0" + minutos : minutos;
-    segundos = segundos < 10 ? "0" + segundos : segundos;
-
-    display.textContent = horas + ":" + minutos + ":" + segundos;
-
-    if(--timer < 0) {
-      timer = duration;
-    }
-
-  }, 1000)
+function onTimesUp() {
+  clearInterval(timerInterval);
+  timerInterval = undefined;
 }
 
-function iniciar() {
-  var duration = 60 * 4;
-  var display = document.querySelector("#tempo");
+function startTimer() {
+  timerInterval = setInterval(() => {
+    timePassed = timePassed += 1;
+    timeLeft = TIME_LIMIT - timePassed;
+    document.getElementById("base-timer-label").innerHTML = formatTime(
+      timeLeft
+    );
+    setCircleDasharray();
+    setRemainingPathColor(timeLeft);
 
-  startTimer(duration, display);
+    if (timeLeft === 0) {
+      onTimesUp();
+      alert("Time Up");
+    }
+  }, 1000);
+}
+
+function formatTime(time) {
+  const minutes = Math.floor(time / 60);
+  let seconds = time % 60;
+
+  if (seconds < 10) {
+    seconds = `0${seconds}`;
+  }
+
+  return `${minutes}:${seconds}`;
+}
+
+function setRemainingPathColor(timeLeft) {
+  const { alert, warning, info } = COLOR_CODES;
+  if (timeLeft <= alert.threshold) {
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.remove(warning.color);
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.add(alert.color);
+  } else if (timeLeft <= warning.threshold) {
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.remove(info.color);
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.add(warning.color);
+  }
+}
+
+function calculateTimeFraction() {
+  const rawTimeFraction = timeLeft / TIME_LIMIT;
+  return rawTimeFraction - (1 / TIME_LIMIT) * (1 - rawTimeFraction);
+}
+
+function setCircleDasharray() {
+  const circleDasharray = `${(
+    calculateTimeFraction() * FULL_DASH_ARRAY
+  ).toFixed(0)} 283`;
+  document
+    .getElementById("base-timer-path-remaining")
+    .setAttribute("stroke-dasharray", circleDasharray);
+}
+
+function starTimer(){
+  if(!timerInterval)
+    startTimer();
+}
+
+function pauseTimer(){
+    onTimesUp();
+}
+function resumeTimer(){
+  console.log(timerInterval);
+    if(!timerInterval)
+    startTimer();
+}
+function resetTimer(){
+    if(timerInterval){
+      onTimesUp()
+    }
+    timePassed = 0;
+    timeLeft = TIME_LIMIT;
+    timerInterval = null;
+  
+    document.getElementById("base-timer-label").innerHTML = formatTime(
+      timeLeft
+    );
+    setCircleDasharray();
+    setRemainingPathColor(timeLeft);
 }
